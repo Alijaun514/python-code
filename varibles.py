5c3df208f6a64cfbdc7ac5470ad28c7eb35ba42a
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-02-22 14:44:54 -0800

    varibles types

---
+++ varibles.py
@@ -0,0 +1,24 @@
+age = 21
+print(age)
+print(type(age))
+
+price = 23.45
+print(price)
+print(type(price))
+
+name = "jaun"
+print(name)
+print(type(name))
+
+user = {"23", "age", "address" }
+print(type(user))
+
+list = ["apple", "banana", "graps"]
+print(list)
+print(type(list))
+
+dis = {
+    "age": "name" , "address" : "home"
+    }
+print(dis)
+print(type(dis))
