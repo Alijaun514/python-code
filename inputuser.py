c7c03d1ce49d7601f0f6ee841c059a5c69970bae
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-02-23 14:33:55 -0800

    input user

---
+++ inputuser.py
@@ -0,0 +1,17 @@
+input()
+print(type(input()))
+
+a = int(input("num1 = "))
+b = int(input("num2 = "))
+sum = a + b 
+print(sum)
+print(type(sum))
+
+a = float(input("F1 ="))
+b = float(input("F2 ="))
+float = a + b 
+print(float)
+print(type(float))
+
+set = {12, 13, 14, 15}
+print(type(set))
