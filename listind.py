6caf5ac83a3a9501cf6058532c0d648c2f9a7b88
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-02-23 14:03:17 -0800

    list of indexing and slicing

---
+++ listind.py
@@ -0,0 +1,25 @@
+set = {"A1", "B2", "C3", "D4"}
+print(set)
+print(type(set))
+
+list = ["white", "green", "blue", "pink"]
+print(list)
+print(type(list))
+#list of ibdexing
+
+print(list[0]) # first item = white
+print(list[-1]) #last item = pink
+
+# slicing
+
+print(list[1 : 3]) # "green", "blue"
+
+# Adding
+    
+
+dis = {
+    "name" : "age" , "Ali Jaun" : "21" ,
+    "class" : "roll no" , " BSCS" : "BSF23004009"
+}
+print(dis)
+print(type(dis))
